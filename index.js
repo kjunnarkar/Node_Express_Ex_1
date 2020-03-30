2841bbd389abbe8907034add453e0ba9b955baec
Kiran Junnarkar <kjunnarkar@yahoo.com>
2020-03-30 16:19:06 -0700

    Added morgan logging

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const http = require('http');
+const morgan = require('morgan');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
+app.use(morgan('dev'));
 
 app.use((req, res, next) => {
     console.log(req.headers)
@@ -16,5 +18,5 @@ app.use((req, res, next) => {
 const server = http.createServer(app);
 
 server.listen(port, () => {
-    console.log(`Server is listening on ${hostname}: ${port}`);
+    console.log(`**********Server is listening on ${hostname}: ${port}**********`);
 });
